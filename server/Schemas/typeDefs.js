0fc356f5c4dc9c6f60fa6810ed9ae5e185d9e2a8
Keiji O <meekunn@gmail.com>
2023-08-31 00:39:41 -0700

    init, started on Schemas, server.js

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,19 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [bookSchema]!
+  }
+  type BookSchema {
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+`;
